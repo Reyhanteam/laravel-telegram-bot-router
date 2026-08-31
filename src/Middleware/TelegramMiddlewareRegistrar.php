df964965401adfbc3bb6d7c0e027cba042acf1cf
REYHANTEAM <hosseinakbarii3776@gmail.com>
2026-08-31 13:59:24 +0330

    feat: add Telegram middleware route registrar

---
+++ src/Middleware/TelegramMiddlewareRegistrar.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ReyhanTeam\TelegramBotRouter\Middleware;
+
+use ReyhanTeam\TelegramBotRouter\TelegramBot;
+
+class TelegramMiddlewareRegistrar
+{
+    public function __construct(protected array $middleware)
+    {
+    }
+
+    public function onCommand(string $command, $callback): void
+    {
+        TelegramBot::addMiddlewareRoute('command', $command, $callback, $this->middleware);
+    }
+
+    public function onText(string $pattern, $callback): void
+    {
+        TelegramBot::addMiddlewareRoute('text', $pattern, $callback, $this->middleware);
+    }
+
+    public function onCallbackQuery($callback): void
+    {
+        TelegramBot::addMiddlewareRoute('callback_query', null, $callback, $this->middleware);
+    }
+}
