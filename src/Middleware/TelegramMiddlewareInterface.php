b24f8050a78d488d10da60e6e07fa0143a955d16
REYHANTEAM <hosseinakbarii3776@gmail.com>
2026-08-31 14:02:44 +0330

    feat: support parameters in middleware contract

--- src/Middleware/TelegramMiddlewareInterface.php
+++ src/Middleware/TelegramMiddlewareInterface.php
@@ -2,9 +2,14 @@
 
 namespace ReyhanTeam\TelegramBotRouter\Middleware;
 
+use Closure;
 use ReyhanTeam\TelegramBotRouter\TelegramUpdate;
 
 interface TelegramMiddlewareInterface
 {
-    public function handle(TelegramUpdate $update, \Closure $next): mixed;
+    public function handle(
+        TelegramUpdate $update,
+        Closure $next,
+        ...$parameters
+    ): mixed;
 }
